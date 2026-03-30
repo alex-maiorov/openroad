d006a27ec52211b65a494d946691b9adab353210
Alex Maiorov <alexander.mmaiorov@gmail.com>
2026-03-30 18:40:32 +0200

    added projection and dot product utilities

--- src/gpl/src/point.h
+++ src/gpl/src/point.h
@@ -2,6 +2,7 @@
 // Copyright (c) 2018-2025, The OpenROAD Authors
 
 #pragma once
+#include <cmath>
 
 namespace gpl {
 
@@ -23,5 +24,28 @@ class FloatPoint
     return FloatPoint(this->x * w, this->y * w);
   }
 };
+
+// class FloatVec
+// {
+// public:
+//
+// };
+
+float floatPointDotProduct(FloatPoint a, FloatPoint b){
+  return (a.x * b.x) + (a.y * b.y);
+}
+
+// Get Projection of src onto the vector A->B
+FloatPoint proj_vector(FloatPoint src, FloatPoint a, FloatPoint b){
+  FloatPoint ab = FloatPoint(b.x - a.x, b.y - a.y); // origin considered at A for this
+  float ab_mag = sqrt(pow(ab.x, 2) + pow(ab.y, 2));
+
+  FloatPoint as = FloatPoint(src.x - a.x, src.y - a.y); // vector from A to src
+  float dot = floatPointDotProduct(as, ab);
+
+  float proj_w = dot / (ab_mag * ab_mag);                // scalar projection factor
+
+  return ab * proj_w;                                    // projection vector along ab
+}
 
 }  // namespace gpl
