726bc85a167b0fcab9a9c17d2fa608dd11656969
Alex Maiorov <alexander.mmaiorov@gmail.com>
2026-04-08 12:07:54 +0200

    refactored point

--- src/gpl/src/point.h
+++ src/gpl/src/point.h
@@ -12,44 +12,15 @@ class FloatPoint
   float x = 0;
   float y = 0;
   FloatPoint() = default;
-  FloatPoint(float x, float y) : x(x), y(y) {}
+  FloatPoint(float x, float y);
 
-  FloatPoint operator+(const FloatPoint& other) const{
-    return FloatPoint(this->x + other.x, this->y + other.y);
-  }
-
-  FloatPoint operator-(const FloatPoint& other) const{
-    return FloatPoint(this->x - other.x, this->y - other.y);
-  }
-
-  // Multiply by scalar.
-  // TODO: Figure out if this is a dangerous overload to do.
-  FloatPoint operator*(const float& w) const{
-    return FloatPoint(this->x * w, this->y * w);
-  }
+  FloatPoint operator+(const FloatPoint& other) const;
+  FloatPoint operator-(const FloatPoint& other) const;
+  FloatPoint operator*(const float& w) const;
 };
 
-// class FloatVec
-// {
-// public:
-//
-// };
+float floatPointDotProduct(FloatPoint a, FloatPoint b);
 
-float floatPointDotProduct(FloatPoint a, FloatPoint b){
-  return (a.x * b.x) + (a.y * b.y);
-}
-
-// Get Projection of src onto the vector A->B
-FloatPoint proj_vector(FloatPoint src, FloatPoint a, FloatPoint b){
-  FloatPoint ab = FloatPoint(b.x - a.x, b.y - a.y); // origin considered at A for this
-  float ab_mag = sqrt(pow(ab.x, 2) + pow(ab.y, 2));
-
-  FloatPoint as = FloatPoint(src.x - a.x, src.y - a.y); // vector from A to src
-  float dot = floatPointDotProduct(as, ab);
-
-  float proj_w = dot / (ab_mag * ab_mag);                // scalar projection factor
-
-  return ab * proj_w;                                    // projection vector along ab
-}
+FloatPoint proj_vector(FloatPoint src, FloatPoint a, FloatPoint b);
 
 }  // namespace gpl
